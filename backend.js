08d044b082b734d1d98304d84c725b393afc7b4c
mrrjew <jklarbi_danquah@st.ug.edu.gh>
2026-02-04 15:00:20 +0000

    card

--- backend.js
+++ backend.js
@@ -83,6 +83,6 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0,0,0,0', () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
